bf5fc3307540343ca6291633edc17c1a8ffb4435
CIS Guru <cisguru@outlook.com>
2025-07-21 14:29:55 -0500

    See CHANGES.md for a list of Revisions.

--- src/json-data/books.js
+++ src/json-data/books.js
@@ -61,31 +61,6 @@ export const Books = [
       "http://www.addyosmani.com/resources/essentialjsdesignpatterns/book/",
   },
   {
-    isbn: "9798602477429",
-    title: "You Don't Know JS Yet",
-    subtitle: "Get Started",
-    author: "Kyle Simpson",
-    published: "2020-01-28T00:00:00.000Z",
-    publisher: "Independently published",
-    pages: 143,
-    description:
-      "The worldwide best selling You Don't Know JS book series is back for a 2nd edition: You Don't Know JS Yet. All 6 books are brand new, rewritten to cover all sides of JS for 2020 and beyond.",
-    website:
-      "https://github.com/getify/You-Dont-Know-JS/tree/2nd-ed/get-started",
-  },
-  {
-    isbn: "9781484200766",
-    title: "Pro Git",
-    subtitle: "Everything you neeed to know about Git",
-    author: "Scott Chacon and Ben Straub",
-    published: "2014-11-18T00:00:00.000Z",
-    publisher: "Apress; 2nd edition",
-    pages: 458,
-    description:
-      "Pro Git (Second Edition) is your fully-updated guide to Git and its usage in the modern world. Git has come a long way since it was first developed by Linus Torvalds for Linux kernel development. It has taken the open source world by storm since its inception in 2005, and this book teaches you how to use it like a pro.",
-    website: "https://git-scm.com/book/en/v2",
-  },
-  {
     isbn: "9781484242216",
     title: "Rethinking Productivity in Software Engineering",
     subtitle: "",
@@ -93,6 +68,18 @@ export const Books = [
     published: "2019-05-11T00:00:00.000Z",
     publisher: "Apress",
     pages: 310,
+    description:
+      'Get the most out of this foundational reference and improve the productivity of your software teams. This open access book collects the wisdom of the 2017 "Dagstuhl" seminar on productivity in software engineering, a meeting of community leaders, who came together with the goal of rethinking traditional definitions and measures of productivity.',
+    website: "https://doi.org/10.1007/978-1-4842-4221-6",
+  },
+  {
+    isbn: "9781635653311",
+    title: "The Men's Health Big Book of Food & Nutrition",
+    subtitle: "",
+    author: "Joel Weber, Editors",
+    published: "2011-05-11T00:00:00.000Z",
+    publisher: "Rodale",
+    pages: 384,
     description:
       'Get the most out of this foundational reference and improve the productivity of your software teams. This open access book collects the wisdom of the 2017 "Dagstuhl" seminar on productivity in software engineering, a meeting of community leaders, who came together with the goal of rethinking traditional definitions and measures of productivity.',
     website: "https://doi.org/10.1007/978-1-4842-4221-6",
